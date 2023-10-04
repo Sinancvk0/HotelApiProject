df6d5eb44e35da17e2eb9b40426527921a8b0dc3
sinan <sinancivak00@gmail.com>
2023-10-04 08:56:11 +0300

    Abstractlar oluşturuldu

---
+++ DataAccess/Abstract/IGenericDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IGenericDal<T> where T : class 
+    {
+        void Insert(T t);
+        void Update(T t);
+
+        void Delete(T t);   
+
+        List<T> GetList(); 
+        
+        T GetById(int id);
+    }
+    
+}
