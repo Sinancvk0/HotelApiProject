71951b90380d6fe13f46edb4ebd0be0748ca4c1b
sinan <sinancivak00@gmail.com>
2023-10-04 08:24:47 +0300

    program.cs  database tanımlandı

--- ApiConsume/HotelApiProject/Program.cs
+++ ApiConsume/HotelApiProject/Program.cs
@@ -1,6 +1,10 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("connstr")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
