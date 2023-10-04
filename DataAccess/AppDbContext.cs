b8a2291aee846a3e39a82c127316840ce7bbec33
sinan <sinancivak00@gmail.com>
2023-10-04 08:22:17 +0300

    AppdbContext

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+
     }
 }
