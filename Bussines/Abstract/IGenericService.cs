21b161d3e4b9b1fa12808d7417496cfaebd126c1
sinan <sinancivak00@gmail.com>
2023-10-04 10:12:17 +0300

    ManagerOluşturuldu

---
+++ Bussines/Abstract/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines.Abstract
+{
+    public interface IGenericService<T> where T : class 
+    {
+        void TInsert(T t);
+        void TUpdate(T t);
+
+        void TDelete(T t);  
+
+        List<T> TGetList();
+
+        T TGetById(int id);
+
+    
+    }
+}
