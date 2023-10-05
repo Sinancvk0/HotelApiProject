024547e3ea841c7cd11d977fbf0fb3ab8531a2e3
sinan <sinancivak00@gmail.com>
2023-10-05 05:00:05 +0300

    Staff Controller oluşturuldu

---
+++ ApiConsume/HotelApiProject/Controllers/StaffController.cs
@@ -0,0 +1,51 @@
+﻿
+using Bussines.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+      private readonly IStaffService _staffService;
+
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddStaff()
+        {
+
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff()
+        {
+
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetStaff()
+        {
+
+            return Ok();
+        }
+    }
+}
