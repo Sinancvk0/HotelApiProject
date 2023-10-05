d5c2a34f85127dab556406f1f8e83c9560e92779
sinan <sinancivak00@gmail.com>
2023-10-05 05:24:01 +0300

    AddScopped işlemleri

--- ApiConsume/HotelApiProject/Program.cs
+++ ApiConsume/HotelApiProject/Program.cs
@@ -1,3 +1,4 @@
+using Bussines.Container;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("connstr")));
+
+ExtensionsDb.ContainerDependecies(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Bussines/Container/ExtensionsDb.cs
@@ -0,0 +1,23 @@
+﻿using Bussines.Abstract;
+using Bussines.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Entity_Framework;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines.Container
+{
+    public class ExtensionsDb
+    {
+        public static void ContainerDependecies(IServiceCollection services)
+        {
+            services.AddScoped<IStaffDal, EfStaff>();
+            services.AddScoped<IStaffService, StaffManager>();
+
+        }
+    }
+}
