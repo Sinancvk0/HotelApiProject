4955851f0ec1876ea9da0b97c6febc2e0f661fb7
sinan <sinancivak00@gmail.com>
2023-10-11 11:24:10 +0300

    .

---
+++ HotelProject/Controllers/AdminLayoutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+
+            return PartialView();   
+        }
+
+    }
+}
---
+++ HotelProject/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
