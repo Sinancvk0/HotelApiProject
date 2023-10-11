b7a39ea9a2c702468e96636f71f61e04c05d24ef
sinan <sinancivak00@gmail.com>
2023-10-11 13:03:19 +0300

    Apiden veri çağrıldı

--- HotelProject/Controllers/StaffController.cs
+++ HotelProject/Controllers/StaffController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            var  client =_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5199/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
---
+++ HotelProject/Models/Staff/StaffViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.Models.Staff
+{
+    public class StaffViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+    }
+}
