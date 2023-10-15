20a6b6ab6562a42be787fcbb1b7df99ccf603c1f
sinan <sinancivak00@gmail.com>
2023-10-16 00:06:39 +0300

    .

--- HotelProject/Controllers/StaffController.cs
+++ HotelProject/Controllers/StaffController.cs
@@ -66,5 +66,29 @@ namespace HotelProject.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+
+        public async Task< IActionResult> UpdateStaff(int id )
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5199/api/staff/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+
+                var values=JsonConvert.DeserializeObject<UpdateViewModel>(jsonData);
+
+                return View(values);
+
+
+            }
+            return View();
+
+        }
+
+
+
     }
 }
