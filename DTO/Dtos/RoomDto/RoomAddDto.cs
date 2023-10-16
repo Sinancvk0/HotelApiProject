f095411950b8ab300e1812ab88f0176989a8ed41
sinan <sinancivak00@gmail.com>
2023-10-16 19:45:18 +0300

    Dto katmanı oluşturuldu

---
+++ DTO/Dtos/RoomDto/RoomAddDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Dtos.RoomDto
+{
+    public class RoomAddDto
+    {
+     
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="Lütfen Oda Numarası Giriniz..")]
+        public string RoomNumber { get; set; }
+        public string CoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Fiyat Giriniz..")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Oda Yatak Sayısı  Giriniz..")]
+        public string BedCount { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Oda Banyo Sayısı Giriniz..")]
+        public string BathCount { get; set; }
+
+        public string Wifi { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ DTO/Dtos/RoomDto/RoomUpdateDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Dtos.RoomDto
+{
+    public class RoomUpdateDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Başlık Giriniz..")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Oda Numarası Giriniz..")]
+        public string RoomNumber { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Görseli Giriniz..")]
+        public string CoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Fiyat Giriniz..")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Oda Yatak Sayısı  Giriniz..")]
+        public string BedCount { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Oda Banyo Sayısı Giriniz..")]
+        public string BathCount { get; set; }
+
+        public string Wifi { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Açıklaması  Giriniz..")]
+        public string Description { get; set; }
+    }
+}
